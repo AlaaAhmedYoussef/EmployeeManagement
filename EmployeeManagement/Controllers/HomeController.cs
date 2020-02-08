f756614a612310195935c477fa666a45cf744f91
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-02-08 11:32:37 +0200

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(ILogger<HomeController> logger, IEmployeeRepository employeeRepository)
+        {
+            _logger = logger;
+            this._employeeRepository = employeeRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var employees = _employeeRepository.GetAllEmployees();
+            return View(employees);
+        }
+
+        public ViewResult Details(int? id)
+        {
+            
+            var HomeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+            return View(HomeDetailsViewModel);
+            //return View(employee);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Employee employee)
+        {
+            var newEmployee = _employeeRepository.Add(employee);
+
+            return RedirectToAction("details", new { id = newEmployee.Id });
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
